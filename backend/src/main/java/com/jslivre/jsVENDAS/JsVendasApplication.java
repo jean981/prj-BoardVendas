7f6cc81577ba5f8587af8103516049330b6cc2c7
root <jean.pereira@educacao.mg.gov.br>
2021-09-26 02:22:04 -0300

    Project Created

---
+++ backend/src/main/java/com/jslivre/jsVENDAS/JsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.jslivre.jsVENDAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsVendasApplication.class, args);
+	}
+
+}
